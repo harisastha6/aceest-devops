8fac9b16ef58dbc51c694c8b35b45f247f510655
harisastha6 <2024tm93586@wilp.bits-pilani.ac.in>
2026-04-26 11:44:54 +0530

    Improved test cases

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,9 +4,16 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+    assert b"ACEest" in response.data
 
 
 def test_add_client():
     client = app.test_client()
     response = client.post("/clients", json={"name": "Hari"})
     assert response.status_code in [200, 201]
+
+
+def test_get_clients():
+    client = app.test_client()
+    response = client.get("/clients")
+    assert response.status_code == 200
